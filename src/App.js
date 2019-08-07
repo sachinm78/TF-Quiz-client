566c9ed1aa5d815bb1de4c4a5289c649daccbd21
sachinm78 <sachinm78@gmail.com>
2019-08-07 10:56:38 -0500

    added NavBar component and imported to App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home'
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <Route exact path='/' component={Home} />
       </div>
     </Router>
