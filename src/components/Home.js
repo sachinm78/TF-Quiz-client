57e64c5e094c686bc37c336ad2ae2aefccd50e82
sachinm78 <sachinm78@gmail.com>
2019-08-07 12:25:27 -0500

    added text, link and img gif to home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import React from "react"
+import { Link } from 'react-router-dom'
 
 export default () => 
     <div>
         <h1>Welcome to TF-Quiz!</h1>
-        <p></p>
+        <p>Select a <Link to = '/categories'>category</Link> and test your knowledge!</p>
+        <img src="http://conaldi.edu.co/wp_ES/wordpress/wp-content/uploads/sites/15/2018/04/Quiz-Animation-Gif-Slower.gif" alt="Quiz Gif"></img>
     </div>
