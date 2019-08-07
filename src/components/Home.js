846d1b474bd6161d22b295994f9ec3a5f073c91f
sachinm78 <sachinm78@gmail.com>
2019-08-07 13:26:16 -0500

    modified text on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,6 @@ import { Link } from 'react-router-dom'
 export default () => 
     <div>
         <h1>Welcome to TF-Quiz!</h1>
-        <p>Select a <Link to = '/categories'>category</Link> and test your knowledge!</p>
+        <h3>Select a <Link to = '/categories'>category</Link> and test your knowledge!</h3>
         <img src="http://conaldi.edu.co/wp_ES/wordpress/wp-content/uploads/sites/15/2018/04/Quiz-Animation-Gif-Slower.gif" alt="Quiz Gif"></img>
     </div>
