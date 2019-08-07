800f9a719d8c1d1d4fd6284ac477b2c4cbedb179
sachinm78 <sachinm78@gmail.com>
2019-08-07 14:36:53 -0500

    changed navbar background color; added div wrapper to quiz img

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,5 +5,7 @@ export default () =>
     <div>
         <h1>Welcome to TF-Quiz!</h1>
         <h3>Select a <Link to = '/categories'>category</Link> and test your knowledge!</h3>
-        <img src="http://conaldi.edu.co/wp_ES/wordpress/wp-content/uploads/sites/15/2018/04/Quiz-Animation-Gif-Slower.gif" alt="Quiz Gif"></img>
+        <div class="quiz-img">
+            <img src="http://conaldi.edu.co/wp_ES/wordpress/wp-content/uploads/sites/15/2018/04/Quiz-Animation-Gif-Slower.gif" alt="Quiz Gif"></img>
+        </div>
     </div>
