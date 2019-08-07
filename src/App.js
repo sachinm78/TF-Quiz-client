0a14411f1c65a2d0ebe97d054bdbbcc6e4e09d91
sachinm78 <sachinm78@gmail.com>
2019-08-07 10:26:53 -0500

    imported react router in App.js, created Home component and route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Home</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path='/' component={Home} />
+      </div>
+    </Router>
   );
 }
 
